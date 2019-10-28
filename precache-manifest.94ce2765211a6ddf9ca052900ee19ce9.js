e23dd477d18e4039a1dbde4cded6c200f1a0e1f1
Super169 <Super169@126.com>
2019-10-29 00:33:31 +0800

    Updates

---
+++ precache-manifest.94ce2765211a6ddf9ca052900ee19ce9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fad7828cf0d970579e5ae4b2622d6981",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7b1f9daf557b056b5066",
+    "url": "/monsters-rolodex/static/css/main.b174a405.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "7b1f9daf557b056b5066",
+    "url": "/monsters-rolodex/static/js/main.fd23a2e5.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
